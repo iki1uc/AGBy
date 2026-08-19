eb5d94688eb02a7a5d82789475926a78778116ec
iki1uc <entertrainer@web.de>
2026-08-19 22:35:47 +0200

    Update ATOM_3D.js

--- AGBy_3D/Core/ATOM_3D.js
+++ AGBy_3D/Core/ATOM_3D.js
@@ -1,5 +1,21 @@
-ATOM_3D {
-    core = stable
-    mass = constant
-    axis = SUS_axis
+export class ATOM_3D {
+    constructor() {
+        this.position = { x: 0, y: 0, z: 0 };
+        this.rotation = { x: 0, y: 0, z: 0 };
+        this.scale = 1;
+    }
+
+    setPosition(x, y, z) {
+        this.position = { x, y, z };
+    }
+
+    rotate(rx, ry, rz) {
+        this.rotation.x += rx;
+        this.rotation.y += ry;
+        this.rotation.z += rz;
+    }
+
+    update() {
+        // später WHIRL, SUS, VECTOR einhängen
+    }
 }
