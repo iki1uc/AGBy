7d244bdde34a2a21926adb634beb8f60b656f5b7
iki1uc <entertrainer@web.de>
2026-08-20 10:49:11 +0200

    Update tornado.move.js

--- tornado.flow/tornado.move.js
+++ tornado.flow/tornado.move.js
@@ -1,1 +1,18 @@
 
+function tornado(pos, speed) {
+
+    // 2 vor
+    pos.y += 2 * speed
+
+    // 1 zurück
+    pos.y -= 1 * speed
+
+    // Verdoppeln
+    pos.x *= 2
+    pos.y *= 2
+
+    // Speed verdoppeln
+    speed *= 2
+
+    return { pos, speed }
+}
