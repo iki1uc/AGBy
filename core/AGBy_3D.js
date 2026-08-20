9112133fef07729a39f098cb58dcf38d5de5d315
iki1uc <entertrainer@web.de>
2026-08-20 12:41:09 +0200

    Create AGBy_3D.js

---
+++ core/AGBy_3D.js
@@ -0,0 +1,20 @@
+// core/AGBy_3D
+
+export const AGBy_3D = {
+
+    base() {
+        return {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0
+        }
+    },
+
+    matrix() {
+        return [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]
+        ]
+    }
+}
