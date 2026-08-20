018d23ad93dcd5f81748c5235c8003e1ef309b34
iki1uc <entertrainer@web.de>
2026-08-20 10:15:45 +0200

    Update locate.js

--- sys/run/locate.js
+++ sys/run/locate.js
@@ -1,1 +1,23 @@
+// sys/run/locate.js
+
+export const locate = {
+
+    position(vector) {
+        return {
+            x: vector.x,
+            y: vector.y,
+            z: vector.z
+        }
+    },
+
+    order(respo) {
+        return respo === "green" ? "stable" :
+               respo === "yellow" ? "soft"   :
+               "critical"
+    },
+
+    depth(build) {
+        return Number(build.orbit.depth).toFixed(2)
+    }
+}
 
